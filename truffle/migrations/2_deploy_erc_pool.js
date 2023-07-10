fc92b1d0e8421826fab3b1a2159b6310b0882dc0
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-07-10 16:12:19 +0200

    Add migrations to deploy pools

---
+++ truffle/migrations/2_deploy_erc_pool.js
@@ -0,0 +1,21 @@
+const LiquidityPool = artifacts.require("LP_ERC")
+const DummyToken = artifacts.require("DummyToken")
+
+module.exports = async function(deployer, network, accounts) {
+    let reserveAddress, safetyModuleAddress, archethicPoolSigner, poolCap, tokenAddress
+    const safeteModuleFeeRate = 5
+
+    if (network == "development") {
+        reserveAddress = accounts[4]
+        safetyModuleAddress = accounts[5]
+        archethicPoolSigner = accounts[6]
+        poolCap = web3.utils.toWei('200')
+
+        await deployer.deploy(DummyToken, web3.utils.toWei('200000'))
+
+        const tokenInstance = await DummyToken.deployed()
+        tokenAddress = tokenInstance.address
+    }
+
+    await deployer.deploy(LiquidityPool, reserveAddress, safetyModuleAddress, safeteModuleFeeRate, archethicPoolSigner, poolCap, tokenAddress);
+}
