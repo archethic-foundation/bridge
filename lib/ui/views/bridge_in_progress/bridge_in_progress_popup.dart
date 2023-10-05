e250cc7ea85300fc5cd49f291c38a9e5354643ce
redDwarf03 <sylvain@archethic.net>
2023-10-05 21:32:01 +0200

    💄 Improve UI for bridge in progress sheet

--- lib/ui/views/bridge_in_progress/bridge_in_progress_popup.dart
+++ lib/ui/views/bridge_in_progress/bridge_in_progress_popup.dart
@@ -1,13 +1,15 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
+import 'package:aebridge/domain/models/failures.dart';
+import 'package:aebridge/ui/views/bridge/bloc/provider.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_circular_step_progress_indicator.dart';
-import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_close_btn.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_contracts.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_current_step.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_error.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_infos.dart';
 import 'package:aebridge/ui/views/bridge_in_progress/components/bridge_in_progress_resume_btn.dart';
-import 'package:aebridge/ui/views/themes/bridge_theme_base.dart';
+import 'package:aebridge/ui/views/util/components/popup_template.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BridgeInProgressPopup {
@@ -18,43 +20,30 @@ class BridgeInProgressPopup {
     return showDialog<void>(
       context: context,
       builder: (context) {
-        return ScaffoldMessenger(
-          child: Builder(
-            builder: (context) {
-              return Scaffold(
-                backgroundColor: Colors.transparent,
-                body: AlertDialog(
-                  backgroundColor: BridgeThemeBase.backgroundPopupColor,
-                  content: SingleChildScrollView(
-                    child: Container(
-                      height: 500,
-                      width: BridgeThemeBase.sizeBoxComponentWidth,
-                      color: Colors.transparent,
-                      padding: const EdgeInsets.only(
-                        top: 10,
-                        left: 30,
-                        right: 30,
-                      ),
-                      child: const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          BridgeInProgressCloseBtn(),
-                          BridgeInProgressInfos(),
-                          BridgeInProgressCircularStepProgressIndicator(),
-                          BridgeInProgressCurrentStep(),
-                          BridgeInProgressError(),
-                          BridgeInProgressContracts(),
-                          Spacer(),
-                          BridgeInProgressResumeBtn(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            },
+        return PopupTemplate(
+          popupTitle: '',
+          popupHeight: 500,
+          popupContent: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              BridgeInProgressInfos(),
+              BridgeInProgressCircularStepProgressIndicator(),
+              BridgeInProgressCurrentStep(),
+              BridgeInProgressError(),
+              BridgeInProgressContracts(),
+              Spacer(),
+              BridgeInProgressResumeBtn(),
+            ],
           ),
+          warningCloseButton: true,
+          warningCloseLabel:
+              AppLocalizations.of(context)!.bridgeProcessInterruptionWarning,
+          warningCloseFunction: () async {
+            await ref
+                .read(BridgeFormProvider.bridgeForm.notifier)
+                .setFailure(const Failure.userRejected());
+          },
         );
       },
     );
