e6d619689b2f419b106ffa8b7ebc5d7947d1b664
redDwarf03 <sylvain@archethic.net>
2023-11-21 17:17:28 +0100

    💄 Add animations

--- lib/ui/views/bridge_in_progress/bridge_in_progress_popup.dart
+++ lib/ui/views/bridge_in_progress/bridge_in_progress_popup.dart
@@ -12,6 +12,8 @@ import 'package:aebridge/ui/views/util/components/popup_close_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
 class BridgeInProgressPopup {
   static Future<void> getDialog(
@@ -41,7 +43,6 @@ class BridgeInProgressPopup {
                                 right: 15,
                                 left: 8,
                               ),
-                              padding: const EdgeInsets.all(20),
                               height: 400,
                               width: BridgeThemeBase.sizeBoxComponentWidth,
                               decoration: BoxDecoration(
@@ -53,17 +54,88 @@ class BridgeInProgressPopup {
                                   ),
                                 ],
                               ),
-                              child: const Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  BridgeInProgressInfosHeader(),
-                                  BridgeInProgressCircularStepProgressIndicator(),
-                                  BridgeInProgressCurrentStep(),
-                                  BridgeInProgressInfosBanner(),
-                                  BridgeInProgressContracts(),
-                                  Spacer(),
-                                  BridgeInProgressResumeBtn(),
+                              child: Stack(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      top: 300,
+                                    ),
+                                    child: Card(
+                                      color: Colors.transparent,
+                                      clipBehavior: Clip.antiAlias,
+                                      elevation: 0,
+                                      margin: EdgeInsets.zero,
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(16),
+                                          bottomRight: Radius.circular(16),
+                                        ),
+                                      ),
+                                      child: WaveWidget(
+                                        config: CustomConfig(
+                                          gradients: [
+                                            [
+                                              ArchethicThemeBase.blue800
+                                                  .withOpacity(0.1),
+                                              ArchethicThemeBase.purple800
+                                                  .withOpacity(0.1),
+                                            ],
+                                            [
+                                              ArchethicThemeBase.blue500
+                                                  .withOpacity(0.1),
+                                              ArchethicThemeBase.purple500
+                                                  .withOpacity(0.1),
+                                            ],
+                                            [
+                                              ArchethicThemeBase.blue300
+                                                  .withOpacity(0.1),
+                                              ArchethicThemeBase.purple300
+                                                  .withOpacity(0.1),
+                                            ],
+                                            [
+                                              ArchethicThemeBase.blue200
+                                                  .withOpacity(0.1),
+                                              ArchethicThemeBase.purple200
+                                                  .withOpacity(0.1),
+                                            ]
+                                          ],
+                                          durations: [
+                                            35000,
+                                            19440,
+                                            10800,
+                                            6000
+                                          ],
+                                          heightPercentages: [
+                                            0.20,
+                                            0.23,
+                                            0.25,
+                                            0.30,
+                                          ],
+                                          gradientBegin: Alignment.bottomLeft,
+                                          gradientEnd: Alignment.topRight,
+                                        ),
+                                        size: Size.infinite,
+                                        waveAmplitude: 0,
+                                      ),
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.all(20),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        BridgeInProgressInfosHeader(),
+                                        BridgeInProgressCircularStepProgressIndicator(),
+                                        BridgeInProgressCurrentStep(),
+                                        BridgeInProgressInfosBanner(),
+                                        BridgeInProgressContracts(),
+                                        Spacer(),
+                                        BridgeInProgressResumeBtn(),
+                                      ],
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
