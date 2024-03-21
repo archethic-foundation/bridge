fde0791af3b0c636421675d60a25678e405d643d
redDwarf03 <sylvain@archethic.net>
2024-03-21 13:47:48 +0100

    🚧 Soon page

--- lib/ui/util/router.dart
+++ lib/ui/util/router.dart
@@ -1,10 +1,4 @@
-import 'package:aebridge/infrastructure/hive/preferences.hive.dart';
-import 'package:aebridge/ui/views/bridge/bloc/state.dart';
-import 'package:aebridge/ui/views/bridge/layouts/bridge_sheet.dart';
-import 'package:aebridge/ui/views/local_history/local_history_sheet.dart';
-import 'package:aebridge/ui/views/refund/layouts/refund_sheet.dart';
 import 'package:aebridge/ui/views/welcome/welcome_screen.dart';
-import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -14,74 +8,12 @@ final rootNavigatorKey = GlobalKey<NavigatorState>();
 final routerProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     navigatorKey: rootNavigatorKey,
-    initialLocation: BridgeSheet.routerPage,
+    initialLocation: WelcomeScreen.routerPage,
     debugLogDiagnostics: true,
     routes: [
       GoRoute(
         path: '/',
         pageBuilder: (context, state) {
-          return const NoTransitionPage(
-            child: BridgeSheet(),
-          );
-        },
-      ),
-      GoRoute(
-        path: BridgeSheet.routerPage,
-        pageBuilder: (context, state) {
-          final helper = QueryParameterHelper();
-          BridgeFormState? bridgeFormState;
-          try {
-            final bridgeFormStateEncoded =
-                state.uri.queryParameters['initialState'];
-            if (bridgeFormStateEncoded != null) {
-              final bridgeFormStateEncodedJson =
-                  helper.decodeQueryParameter(bridgeFormStateEncoded);
-              bridgeFormState = BridgeFormState.fromJson(
-                bridgeFormStateEncodedJson,
-              );
-            }
-          } catch (e)
-          // ignore: empty_catches
-          {}
-
-          return NoTransitionPage(
-            child: BridgeSheet(
-              initialState: bridgeFormState,
-            ),
-          );
-        },
-      ),
-      GoRoute(
-        path: LocalHistorySheet.routerPage,
-        pageBuilder: (context, state) {
-          return const NoTransitionPage(
-            child: LocalHistorySheet(),
-          );
-        },
-      ),
-      GoRoute(
-        path: RefundSheet.routerPage,
-        pageBuilder: (context, state) {
-          String? htlcAddress;
-          try {
-            final helper = QueryParameterHelper();
-            final htlcAddressEncoded = state.uri.queryParameters['htlcAddress'];
-            if (htlcAddressEncoded != null) {
-              htlcAddress = helper.decodeQueryParameter(htlcAddressEncoded);
-            }
-          } catch (e)
-          // ignore: empty_catches
-          {}
-          return NoTransitionPage(
-            child: RefundSheet(
-              htlcAddress: htlcAddress,
-            ),
-          );
-        },
-      ),
-      GoRoute(
-        path: WelcomeScreen.routerPage,
-        pageBuilder: (BuildContext context, GoRouterState state) {
           return const NoTransitionPage(
             child: WelcomeScreen(),
           );
@@ -89,13 +21,7 @@ final routerProvider = Provider<GoRouter>((ref) {
       ),
     ],
     redirect: (context, state) async {
-      await HivePreferencesDatasource.getInstance().then((value) {
-        if (value.isFirstConnection()) {
-          if (context.mounted) return WelcomeScreen.routerPage;
-        }
-      });
-
-      return null;
+      return WelcomeScreen.routerPage;
     },
     errorBuilder: (context, state) => const WelcomeScreen(),
   );
--- lib/ui/views/welcome/components/welcome_bridge_btn.dart
+++ lib/ui/views/welcome/components/welcome_bridge_btn.dart
@@ -1,13 +1,8 @@
-/// SPDX-License-Identifier: AGPL-3.0-or-later
-import 'package:aebridge/application/session/provider.dart';
-import 'package:aebridge/ui/views/bridge/layouts/bridge_sheet.dart';
 import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
     as aedappfm;
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class WelcomeBridgeBtn extends ConsumerStatefulWidget {
   const WelcomeBridgeBtn({
@@ -45,15 +40,7 @@ class WelcomeBridgeBtnState extends ConsumerState<WelcomeBridgeBtn> {
                   side: MaterialStateProperty.all(BorderSide.none),
                   overlayColor: MaterialStateProperty.all(Colors.transparent),
                 ),
-                onPressed: () async {
-                  final sessionNotifier =
-                      ref.read(SessionProviders.session.notifier);
-                  await sessionNotifier.cancelAllWalletsConnection();
-                  if (!context.mounted) return;
-                  context.go(
-                    BridgeSheet.routerPage,
-                  );
-                },
+                onPressed: () async {},
                 child: Container(
                   alignment: Alignment.center,
                   height: 50,
@@ -70,7 +57,7 @@ class WelcomeBridgeBtnState extends ConsumerState<WelcomeBridgeBtn> {
                     ],
                   ),
                   child: Text(
-                    AppLocalizations.of(context)!.btn_bridge,
+                    'Soon',
                     style: TextStyle(
                       color: Theme.of(context).textTheme.labelMedium!.color,
                       fontSize: aedappfm.Responsive.fontSizeFromValue(
