8927d50514a143cd62ce78589bfb1729d5456432
redDwarf03 <sylvain@archethic.net>
2024-04-26 00:32:56 +0200

    new failure

--- lib/util/faucet_util.dart
+++ lib/util/faucet_util.dart
@@ -1,27 +1,39 @@
 class FaucetUtil {
-  static Uri getUrl(Map<String, dynamic> queryParameters) {
+  static ({Uri uri, bool executeCatch}) getUrl(
+    Map<String, dynamic> queryParameters,
+  ) {
     if (Uri.base.toString().toLowerCase().contains('bridge.archethic')) {
-      return Uri.https(
-        'faucet.bridge.archethic.net',
-        '/api/faucet',
-        queryParameters,
+      return (
+        uri: Uri.https(
+          'faucet.bridge.archethic.net',
+          '/api/faucet',
+          queryParameters,
+        ),
+        executeCatch: true
       );
     } else {
       if (Uri.base
           .toString()
           .toLowerCase()
           .contains('bridge.testnet.archethic')) {
-        return Uri.https(
-          'faucet.bridge.testnet.archethic.net',
-          '/api/faucet',
-          queryParameters,
+        return (
+          uri: Uri.https(
+            'faucet.bridge.testnet.archethic.net',
+            '/api/faucet',
+            queryParameters,
+          ),
+          executeCatch: true
         );
       } else {
         // TODO(redwwarf03): MAINNET
-        return Uri.https(
-          'faucet.bridge.archethic.net',
-          '/api/faucet',
-          queryParameters,
+        return (
+          uri: Uri.https(
+            'faucet.bridge.testnet.archethic.net',
+            '/api/faucet',
+            queryParameters,
+          ),
+          // Because of CORS policy
+          executeCatch: true
         );
       }
     }
