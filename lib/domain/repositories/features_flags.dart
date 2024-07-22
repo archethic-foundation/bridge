6b66d877d1db846ae9104b81192ae118a481267d
redDwarf03 <sylvain@archethic.net>
2024-07-22 10:18:32 +0200

    Go to maintenance

--- lib/domain/repositories/features_flags.dart
+++ lib/domain/repositories/features_flags.dart
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 /// Simply activate/deactivate features.
 class FeatureFlags {
-  static const inMaintenance = false;
+  static const inMaintenance = true;
 }
