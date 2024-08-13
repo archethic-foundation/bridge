7ca85f44308c3a333bde6bda712ca37964549f77
redDwarf03 <sylvain@archethic.net>
2024-08-13 19:22:30 +0200

    feat: :sparkles: Add troubles menu

---
+++ lib/ui/views/main_screen/layouts/troubles_popup.dart
@@ -0,0 +1,178 @@
+import 'package:aebridge/ui/views/local_history/local_history_sheet.dart';
+import 'package:aebridge/ui/views/refund/layouts/refund_sheet.dart';
+import 'package:aebridge/ui/views/util/app_styles.dart';
+import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
+    as aedappfm;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class TroublesPopup extends ConsumerWidget {
+  const TroublesPopup({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () {
+        context.pop();
+      },
+      child: Scaffold(
+        backgroundColor: Colors.transparent.withAlpha(120),
+        body: AlertDialog(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          content: Container(
+            margin: const EdgeInsets.only(
+              top: 30,
+              right: 15,
+              left: 8,
+            ),
+            padding: const EdgeInsets.all(20),
+            width: MediaQuery.of(context).size.width * 0.6,
+            decoration: BoxDecoration(
+              color: aedappfm.AppThemeBase.backgroundPopupColor,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: const <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                ),
+              ],
+            ),
+            child: aedappfm.ArchethicScrollbar(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SelectableText(
+                    AppLocalizations.of(context)!.havingTroubleHeader,
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: aedappfm.AppThemeBase.secondaryColor,
+                        ),
+                  ),
+                  const SizedBox(height: 30),
+                  Opacity(
+                    opacity: AppTextStyles.kOpacityText,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        _line(
+                            context,
+                            AppLocalizations.of(context)!.troubleItem1Title,
+                            AppLocalizations.of(context)!.troubleItem1Desc,
+                            'FAQ', () {
+                          launchUrl(
+                            Uri.parse(
+                              'https://wiki.archethic.net/FAQ/bridge-2-ways',
+                            ),
+                          );
+                        }),
+                        _line(
+                            context,
+                            AppLocalizations.of(context)!.troubleItem2Title,
+                            AppLocalizations.of(context)!.troubleItem2Desc,
+                            'tutorials', () {
+                          launchUrl(
+                            Uri.parse(
+                              'https://wiki.archethic.net/participate/bridge/usage',
+                            ),
+                          );
+                        }),
+                        _line(
+                            context,
+                            AppLocalizations.of(context)!.troubleItem3Title,
+                            AppLocalizations.of(context)!.troubleItem3Desc,
+                            'history', () {
+                          context
+                            ..pop()
+                            ..go(LocalHistorySheet.routerPage);
+                        }),
+                        _line(
+                            context,
+                            AppLocalizations.of(context)!.troubleItem4Title,
+                            AppLocalizations.of(context)!.troubleItem4Desc,
+                            'assets', () {
+                          context
+                            ..pop()
+                            ..go(RefundSheet.routerPage);
+                        }),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _line(
+    BuildContext context,
+    String title,
+    String desc,
+    String keyword,
+    VoidCallback link,
+  ) {
+    final textStyleDesc = Theme.of(context)
+        .textTheme
+        .bodyLarge!
+        .copyWith(fontWeight: FontWeight.w200);
+    final textStyleTitle = Theme.of(context)
+        .textTheme
+        .bodyLarge!
+        .copyWith(fontWeight: FontWeight.w700);
+
+    final beforeKeyword = desc.split(keyword)[0];
+    final afterKeyword = desc.split(keyword)[1];
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 2, right: 4),
+            child: Icon(
+              Icons.arrow_forward_ios,
+              color: aedappfm.AppThemeBase.secondaryColor,
+              size: 14,
+            ),
+          ),
+          Expanded(
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: '$title ',
+                    style: textStyleTitle,
+                  ),
+                  TextSpan(
+                    text: beforeKeyword,
+                    style: textStyleDesc,
+                  ),
+                  TextSpan(
+                    text: keyword,
+                    style: textStyleDesc.copyWith(
+                      color: aedappfm.AppThemeBase.secondaryColor,
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()..onTap = link,
+                  ),
+                  TextSpan(
+                    text: afterKeyword,
+                    style: textStyleDesc,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
