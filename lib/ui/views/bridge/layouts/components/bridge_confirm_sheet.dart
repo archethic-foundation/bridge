31d889086be7004e14153824ed212063c16fe804
redDwarf03 <sylvain@archethic.net>
2024-10-09 20:02:38 +0200

    fix: :bug: Fix Incorrect use of ParentDataWidget

--- lib/ui/views/bridge/layouts/components/bridge_confirm_sheet.dart
+++ lib/ui/views/bridge/layouts/components/bridge_confirm_sheet.dart
@@ -33,153 +33,151 @@ class BridgeConfirmSheetState extends ConsumerState<BridgeConfirmSheet> {
       return const SizedBox.shrink();
     }
 
-    return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          aedappfm.ButtonConfirmBack(
-            title: AppLocalizations.of(context)!.bridgeConfirmTitle,
-            onPressed: bridge.tokenToBridge == null
-                ? null
-                : () {
-                    ref.read(bridgeFormNotifierProvider.notifier)
-                      ..setBridgeProcessStep(
-                        AppLocalizations.of(context)!,
-                        aedappfm.ProcessStep.form,
-                      )
-                      ..setMessageMaxHalfUCO(false);
-                  },
-          ),
-          const SizedBox(height: 15),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        aedappfm.ButtonConfirmBack(
+          title: AppLocalizations.of(context)!.bridgeConfirmTitle,
+          onPressed: bridge.tokenToBridge == null
+              ? null
+              : () {
+                  ref.read(bridgeFormNotifierProvider.notifier)
+                    ..setBridgeProcessStep(
+                      AppLocalizations.of(context)!,
+                      aedappfm.ProcessStep.form,
+                    )
+                    ..setMessageMaxHalfUCO(false);
+                },
+        ),
+        const SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SelectableText(
+              AppLocalizations.of(context)!.bridge_blockchain_from_lbl,
+            ),
+            const SizedBox(width: 8),
+            BlockchainLabel(
+              chainId: bridge.blockchainFrom!.chainId,
+            ),
+          ],
+        ),
+        const SizedBox(height: 3),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SelectableText(
+              AppLocalizations.of(context)!.bridge_blockchain_to_lbl,
+            ),
+            const SizedBox(width: 8),
+            BlockchainLabel(
+              chainId: bridge.blockchainTo!.chainId,
+            ),
+          ],
+        ),
+        const SizedBox(height: 3),
+        if (bridge.tokenToBridge != null)
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               SelectableText(
-                AppLocalizations.of(context)!.bridge_blockchain_from_lbl,
+                AppLocalizations.of(context)!.bridge_token_amount_lbl,
               ),
-              const SizedBox(width: 8),
-              BlockchainLabel(
-                chainId: bridge.blockchainFrom!.chainId,
+              FutureBuilder<String>(
+                future: FiatValue().display(
+                  ref,
+                  bridge.tokenToBridge!.symbol,
+                  bridge.tokenToBridgeAmount,
+                ),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return SelectableText(
+                      '${bridge.tokenToBridgeAmount.formatNumber(precision: 8)} ${bridge.tokenToBridge!.symbol} ${snapshot.data}',
+                    );
+                  }
+                  return const SizedBox.shrink();
+                },
               ),
             ],
           ),
-          const SizedBox(height: 3),
+        const SizedBox(
+          height: 3,
+        ),
+        if (bridge.targetAddress.isNotEmpty)
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SelectableText(
-                AppLocalizations.of(context)!.bridge_blockchain_to_lbl,
+              Flexible(
+                child: SelectableText(
+                  AppLocalizations.of(context)!.bridge_target_address_lbl,
+                ),
               ),
-              const SizedBox(width: 8),
-              BlockchainLabel(
+              const SizedBox(
+                width: 20,
+              ),
+              FormatAddressLinkCopy(
+                address: bridge.targetAddress,
                 chainId: bridge.blockchainTo!.chainId,
+                reduceAddress: true,
               ),
             ],
           ),
-          const SizedBox(height: 3),
-          if (bridge.tokenToBridge != null)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SelectableText(
-                  AppLocalizations.of(context)!.bridge_token_amount_lbl,
-                ),
-                FutureBuilder<String>(
-                  future: FiatValue().display(
-                    ref,
-                    bridge.tokenToBridge!.symbol,
-                    bridge.tokenToBridgeAmount,
-                  ),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return SelectableText(
-                        '${bridge.tokenToBridgeAmount.formatNumber(precision: 8)} ${bridge.tokenToBridge!.symbol} ${snapshot.data}',
-                      );
-                    }
-                    return const SizedBox.shrink();
-                  },
-                ),
-              ],
-            ),
-          const SizedBox(
-            height: 3,
-          ),
-          if (bridge.targetAddress.isNotEmpty)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: SelectableText(
-                    AppLocalizations.of(context)!.bridge_target_address_lbl,
-                  ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                FormatAddressLinkCopy(
-                  address: bridge.targetAddress,
-                  chainId: bridge.blockchainTo!.chainId,
-                  reduceAddress: true,
-                ),
-              ],
-            ),
-          const SizedBox(
-            height: 10,
-          ),
-          const BridgeConfirmSheetFees(),
-          if (bridge.messageMaxHalfUCO)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: SizedBox(
-                child: aedappfm.InfoBanner(
-                  AppLocalizations.of(context)!.bridgeConfirmMessageMaxHalfUCO,
-                  aedappfm.InfoBannerType.request,
-                ),
+        const SizedBox(
+          height: 10,
+        ),
+        const BridgeConfirmSheetFees(),
+        if (bridge.messageMaxHalfUCO)
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: SizedBox(
+              child: aedappfm.InfoBanner(
+                AppLocalizations.of(context)!.bridgeConfirmMessageMaxHalfUCO,
+                aedappfm.InfoBannerType.request,
               ),
             ),
-          const SizedBox(
-            height: 10,
           ),
-          const Spacer(),
-          if (bridge.consentDateTime == null)
-            aedappfm.ConsentToCheck(
-              consentChecked: consentChecked,
-              onToggleConsent: (newValue) {
-                setState(() {
-                  consentChecked = newValue!;
-                });
-              },
-              uriPrivacyPolicy: kURIPrivacyPolicy,
-              uriTermsOfUse: kURITermsOfUse,
-            )
-          else
-            aedappfm.ConsentAlready(
-              consentDateTime: bridge.consentDateTime!,
-              uriPrivacyPolicy: kURIPrivacyPolicy,
-              uriTermsOfUse: kURITermsOfUse,
-            ),
-          aedappfm.ButtonConfirm(
-            labelBtn: AppLocalizations.of(context)!.btn_confirm_bridge,
-            disabled: !consentChecked && bridge.consentDateTime == null,
-            onPressed: () async {
-              unawaited(
-                ref
-                    .read(bridgeFormNotifierProvider.notifier)
-                    .bridge(AppLocalizations.of(context)!, ref),
-              );
-
-              if (!context.mounted) return;
-              await BridgeInProgressPopup.getDialog(
-                context,
-                ref,
-              );
+        const SizedBox(
+          height: 10,
+        ),
+        const Spacer(),
+        if (bridge.consentDateTime == null)
+          aedappfm.ConsentToCheck(
+            consentChecked: consentChecked,
+            onToggleConsent: (newValue) {
+              setState(() {
+                consentChecked = newValue!;
+              });
             },
+            uriPrivacyPolicy: kURIPrivacyPolicy,
+            uriTermsOfUse: kURITermsOfUse,
+          )
+        else
+          aedappfm.ConsentAlready(
+            consentDateTime: bridge.consentDateTime!,
+            uriPrivacyPolicy: kURIPrivacyPolicy,
+            uriTermsOfUse: kURITermsOfUse,
           ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+        aedappfm.ButtonConfirm(
+          labelBtn: AppLocalizations.of(context)!.btn_confirm_bridge,
+          disabled: !consentChecked && bridge.consentDateTime == null,
+          onPressed: () async {
+            unawaited(
+              ref
+                  .read(bridgeFormNotifierProvider.notifier)
+                  .bridge(AppLocalizations.of(context)!, ref),
+            );
+
+            if (!context.mounted) return;
+            await BridgeInProgressPopup.getDialog(
+              context,
+              ref,
+            );
+          },
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
