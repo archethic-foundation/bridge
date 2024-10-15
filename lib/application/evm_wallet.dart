89d69d8169120d6a9e048a933773b0fae5d0df19
redDwarf03 <sylvain@archethic.net>
2024-10-16 00:45:15 +0200

    chore: :wrench: Restrict wallets list

--- lib/application/evm_wallet.dart
+++ lib/application/evm_wallet.dart
@@ -58,6 +58,21 @@ class EVMWalletProvider extends ChangeNotifier with EVMBridgeProcessMixin {
             .firstWhere((element) => element.chainId == chainId)
             .providerEndpoint,
       ),
+      // https://explorer.walletconnect.com/
+      includeWalletIds: [
+        'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // Metamask
+        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // TrustWallet
+        '971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709', // OKSWallet
+        '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4', // Binance WEB3 Wallet
+        'c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a', // Uniswap
+        'a9751f17a3292f2d1493927f0555603d69e9a3fcbcdf5626f01b49afa21ced91', // Frame
+        '0b415a746fb9ee99cce155c2ceca0c6f6061b1dbca2d722b3ba16381d0562150', // SafePAL
+        '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
+        'e9ff15be73584489ca4a66f64d32c4537711797e30b6660dbcb71ea72a42b1f4', // Exodus
+        'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
+        'a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393', // Phantom
+        '225affb176778569276e484e1b92637ad061b01e13a048b35a9d280c3b58970f', // Safe
+      ],
     );
 
     isInit = true;
