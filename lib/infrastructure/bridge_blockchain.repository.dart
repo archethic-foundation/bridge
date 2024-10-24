e6b664a6129d9b1f2c0e0a2db6da42fc3c021952
Chralu <contact@charly-caulet.net>
2024-10-24 17:01:13 +0200

    fix: :bug: Regerssion on Archethic blockchain auto-selection.

--- lib/infrastructure/bridge_blockchain.repository.dart
+++ lib/infrastructure/bridge_blockchain.repository.dart
@@ -55,6 +55,6 @@ class BridgeBlockchainsRepositoryImpl implements BridgeBlockchainsRepository {
     BridgeBlockchain evmBlockchain,
   ) async =>
       blockchainsList.firstWhereOrNull(
-        (element) => element.env == evmBlockchain.env && !element.isArchethic,
+        (element) => element.env == evmBlockchain.env && element.isArchethic,
       );
 }
