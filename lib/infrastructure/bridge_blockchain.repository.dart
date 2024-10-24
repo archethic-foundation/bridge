07508ad51c5f21285ecbacf1564afbccb6f76c22
Chralu <contact@charly-caulet.net>
2024-10-24 15:27:45 +0200

    fix: :bug: Prevent desctructive operations on Collections.

--- lib/infrastructure/bridge_blockchain.repository.dart
+++ lib/infrastructure/bridge_blockchain.repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:aebridge/domain/models/bridge_blockchain.dart';
 import 'package:aebridge/domain/repositories/bridge_blockchain.repository.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/services.dart';
 
 class BridgeBlockchainsRepositoryImpl implements BridgeBlockchainsRepository {
@@ -28,39 +29,32 @@ class BridgeBlockchainsRepositoryImpl implements BridgeBlockchainsRepository {
   List<BridgeBlockchain> getBlockchainsList(
     List<BridgeBlockchain> blockchainsList,
   ) {
-    blockchainsList.sort((a, b) {
-      final compareEnv = a.env.compareTo(b.env);
-      if (compareEnv != 0) {
-        return compareEnv;
-      } else {
-        return a.name.compareTo(b.name);
-      }
-    });
-    return blockchainsList;
+    final sortedList = [...blockchainsList]..sort((a, b) {
+        final compareEnv = a.env.index.compareTo(b.env.index);
+        if (compareEnv != 0) {
+          return compareEnv;
+        } else {
+          return a.name.compareTo(b.name);
+        }
+      });
+    return sortedList;
   }
 
   @override
   Future<BridgeBlockchain?> getBlockchainFromChainId(
     List<BridgeBlockchain> blockchainsList,
     int chainId,
-  ) async {
-    return blockchainsList.singleWhere((element) => element.chainId == chainId);
-  }
+  ) async =>
+      blockchainsList.firstWhereOrNull(
+        (element) => element.chainId == chainId,
+      );
 
   @override
   Future<BridgeBlockchain?> getArchethicBlockchainFromEVM(
     List<BridgeBlockchain> blockchainsList,
     BridgeBlockchain evmBlockchain,
-  ) async {
-    blockchainsList
-      ..removeWhere(
-        (element) => element.env != evmBlockchain.env,
-      )
-      ..removeWhere((element) => element.isArchethic == false);
-    if (blockchainsList.length == 1) {
-      return blockchainsList[0];
-    }
-
-    return null;
-  }
+  ) async =>
+      blockchainsList.firstWhereOrNull(
+        (element) => element.env == evmBlockchain.env && !element.isArchethic,
+      );
 }
